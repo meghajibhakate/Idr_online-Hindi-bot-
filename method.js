75687a87feeb177bf38bfaaeaeff45de2e4756a7
meghajibhakate <meghajibhakate18@gmail.com>
2023-02-01 11:58:15 +0530

    latestnum

--- method.js
+++ method.js
@@ -3,30 +3,36 @@ import Parser from "rss-parser";
 import { bot } from "./bot.js";
 import fs from 'fs';
 import { InlineKeyboard } from 'grammy';
+import{updatePost, getPost} from "./db.js"
 
 
 let parser = new Parser()
 
 
-export async function getPosts() {
+export async function getWebsitePosts() {
   let feed = await parser.parseURL('https://idronline.org/feed/');
-  // let keys = Object.keys(feed);
   // console.log(feed.items);
   let allpost = feed.items.length
   let latestnum = allpost
   console.log("latestnum    " + latestnum);
 
-  let data = fs.readFileSync("./file.json")
-  let jsondata = JSON.parse(data)
-  let lastpost = jsondata.totalpost;
+  // let data = fs.readFileSync("./file.json")
+  // let jsondata = JSON.parse(data)
+  let data = await getPost();
+  let lastpost = data.totalpost;
   let updateNum = 0
 
   if (latestnum != lastpost) {
     updateNum = latestnum - lastpost;
     console.log(updateNum);
-    jsondata.totalpost = latestnum;
-    fs.writeFileSync("./file.json", JSON.stringify(jsondata))
+    // fs.writeFileSync("./file.json", JSON.stringify(jsondata))
+    await updatePost(latestnum);
   }
+
+
+
+
+
 
   for (let i = 0; i < updateNum; i++) {
     await bot.api.sendMessage(5636179119, ` <b><a href="${feed.items[i].link}">${feed.items[i].title}</a></b> 
