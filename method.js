b11b1db88cad8d2eed844d533f5ee843612f0b1d
meghajibhakate <meghajibhakate18@gmail.com>
2023-02-02 11:42:55 +0530

    calling the function

--- method.js
+++ method.js
@@ -3,7 +3,7 @@ import Parser from "rss-parser";
 import { bot } from "./bot.js";
 import fs from 'fs';
 import { InlineKeyboard } from 'grammy';
-import{updatePost, getPost} from "./db.js"
+import { updatePost, getPost, getChats } from "./db.js"
 
 
 let parser = new Parser()
@@ -32,18 +32,38 @@ export async function getWebsitePosts() {
 
 
 
+  const allChats = await getChats();
+  console.log(allChats)
 
   for (let i = 0; i < updateNum; i++) {
-    await bot.api.sendMessage(5636179119, ` <b><a href="${feed.items[i].link}">${feed.items[i].title}</a></b> 
-    \n by:- <i>${feed.items[i].creator}</i> 
-    \n\n <i>${feed.items[i].content.replace(/<[^>]*>?/gm, '').slice(0, 300)}...<a href="${feed.items[i].link}">Read More</a></i>`, {
-      parse_mode: "HTML",
-      disable_web_page_preview: true,
-      reply_markup: new InlineKeyboard().url(
-        "Read Post",
-        `${feed.items[i].link}`,
+    for (let chat of chatsArray) {
+      // taking the all chat id's here
+      await bot.api.sendMessage(chat._id, ` <b><a href="${feed.items[i].link}">${feed.items[i].title}</a></b> 
+                \n by:- <i>${feed.items[i].creator}</i> 
+                \n\n <i>${feed.items[i].content.replace(/<[^>]*>?/gm, '').slice(0, 300)}...
+                <a href="${feed.items[i].link}">Read More</a></i>`,
+        {
+          parse_mode: "HTML",
+          disable_web_page_preview: true,
+          reply_markup: new InlineKeyboard().url(
+            "Read Post",
+            `${feed.items[i].link}`,
+          )
+        }
       )
-    })
+    }
+
+
+    // await bot.api.sendMessage(5636179119, ` <b><a href="${feed.items[i].link}">${feed.items[i].title}</a></b> 
+    // \n by:- <i>${feed.items[i].creator}</i> 
+    // \n\n <i>${feed.items[i].content.replace(/<[^>]*>?/gm, '').slice(0, 300)}...<a href="${feed.items[i].link}">Read More</a></i>`, {
+    //   parse_mode: "HTML",
+    //   disable_web_page_preview: true,
+    //   reply_markup: new InlineKeyboard().url(
+    //     "Read Post",
+    //     `${feed.items[i].link}`,
+    //   )
+    // })
 
 
 
